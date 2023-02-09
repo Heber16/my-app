cb7e90e6b44f268c9a9301a41d9bbabb74a23c09
Heber <heber1514@hotmail.com>
2023-02-08 19:37:29 -0600

    Nombre del proyecto: my-app. Descripcion: Version final del proyecto my-app

---
+++ src/main/java/com/heb/myapp/entity/TestEntity.java
@@ -0,0 +1,25 @@
+/**
+ * 
+ */
+package com.heb.myapp.entity;
+
+/**
+ * @author Heber Mauricio Medina Miranda
+ *
+ */
+public class TestEntity {
+	/*
+	 * Propiedad de prueba para la muestra de compilacion del archivo TestEntity
+	 * */
+	
+	public String getNombre() {
+		return nombre;
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+	private String nombre;
+
+}
